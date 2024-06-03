85ead1136a58729a55084bf9e53480ee806c8459
Artur N <arturnunespe@gmail.com>
2024-06-03 17:55:20 -0300

    Clear vunerability warnings

--- components/BigDumbMarkdown.tsx
+++ components/BigDumbMarkdown.tsx
@@ -1,10 +1,11 @@
+import escapeHTML from 'escape-html'
 import markdownStyles from './markdown-styles.module.css'
 
 export default function BigDumbMarkdown({ content }: { content: string }) {
   return (
     <div className="flex flex-col items-center py-8">
       <div className={markdownStyles['markdown']}>
-        <div dangerouslySetInnerHTML={{ __html: content }} />
+        <div dangerouslySetInnerHTML={{ __html: escapeHTML(content) }} />
       </div>
     </div>
   )
