315c61afdd18e9216031bcdff877e54e114829ba
Artur <arturnunespe@gmail.com>
2024-09-29 19:45:13 -0300

    fix: address font not always loading bug

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,19 +9,25 @@ interface Props {
   children: ReactNode
 }
 
-const inter = Inter({
-  subsets: ['latin'],
-})
+const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false })
 
 const LayoutWrapper = ({ children }: Props) => {
   return (
-    <SectionContainer>
-      <div className={`${inter.className} flex h-screen flex-col justify-between`}>
-        <Header />
-        <main className="grow">{children}</main>
-        <Footer />
-      </div>
-    </SectionContainer>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+
+      <SectionContainer>
+        <div className={`${inter.className} flex h-screen flex-col justify-between`}>
+          <Header />
+          <main className="grow">{children}</main>
+          <Footer />
+        </div>
+      </SectionContainer>
+    </>
   )
 }
 
