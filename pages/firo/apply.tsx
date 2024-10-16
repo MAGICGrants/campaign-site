39b13ccc0e49a06b5092968cd8d3bd410a4c74fb
Justin Ehrenhofer <12520755+SamsungGalaxyPlayer@users.noreply.github.com>
2024-10-16 16:25:22 -0500

    Form updates

--- pages/firo/apply.tsx
+++ pages/firo/apply.tsx
@@ -41,80 +41,19 @@ export default function Apply() {
             free and open source projects.
           </p>
           <p>
-            The MAGIC Firo Fund is offering a grant program and fundraising platform to support Firo
-            research and development, especially relating to privacy, security, user experience, and
-            efficiency. Proposals can be related to the Firo protocol directly, or they can be
-            related to other areas of the Firo ecosystem. For research projects, please refer to
-            special instructions
-            <Link href="/apply_research"> here</Link>.
-          </p>
-          <h2>Proposal Evaluation Criteria</h2>
-          <div>
-            Submitted proposals will be evaluated by the committee based on the following criteria:
-            <ul>
-              <li>
-                <b>Impact:</b> The proposal should have a clear impact on the Firo Project.
-              </li>
-              <li>
-                <b>Originality:</b> The proposal should be original and not a rehash of existing
-                work.
-              </li>
-              <li>
-                <b>Feasibility:</b> The proposal should be feasible to complete within the proposed
-                time frame.
-              </li>
-              <li>
-                <b>Quality:</b> The proposal should be well-written and well-organized.
-              </li>
-              <li>
-                <b>Relevance:</b> The proposal should be relevant to the Firo Project.
-              </li>
-            </ul>
-          </div>
-          <h2 id="Eligibility">Eligibility</h2>
-          <p>
-            All qualified researchers are eligible to apply, regardless of educational attainment or
-            occupation. However, as a nonprofit organization registered under U.S. tax laws, MAGIC
-            Grants is required to comply with certain laws when disbursing funds to grant
-            recipients. Grant recipients must complete a Due Diligence checklist, which are the last
-            two pages of{' '}
-            <a href="https://magicgrants.org/funds/MAGIC%20Fund%20Grant%20Disbursement%20Process%20and%20Requirements.pdf">
-              this document
-            </a>
-            . This includes the collection of your ID and tax information. We will conduct sanctions
-            checks.
-          </p>
-          <h2>How to Submit a Proposal</h2>
-          <p>
-            To submit a proposal, please complete the form below or create an issue on{' '}
-            {/* REVIEW START */}
-            <a href="https://github.com/MAGICGrants/Monero-Fund/issues/new?assignees=&labels=&template=grant-application.md&title=[Grant+Title]">
-              Github
-            </a>
-            {/* REVIEW END */}. Applicants are free to use their legal name or a pseudonym at this
-            step, although note the{' '}
-            <a href="#Eligibility">
-              <b>Eligibility</b>
-            </a>{' '}
-            section. You can choose to apply for a direct grant from the MAGIC Firo Fund&#39;s
-            General Fund and/or request that your project be listed on donate.magicgrants.org to
-            raise funds from Firo community members.
-          </p>
-          <p>
-            Please note this form is not considered confidential and is effectively equivalent to a
-            public GitHub issue. In order to reach out privately, please send an email to{' '}
-            {fundSlugToRecipientEmail[fundSlug]}.
+            Please fill in your contact information and your project idea below so that we can process
+            your inquiry.
           </p>
         </div>
 
         <label className="checkbox">
           <input type="checkbox" {...register('general_fund')} />
-          Apply to receive a grant from the Magic Firo Fund.
+          Apply to receive a grant from the MAGIC Firo Fund.
         </label>
 
         <label className="checkbox">
           <input type="checkbox" {...register('explore_page')} />
-          Apply for project to be listed on the Firo Fund Donation Page.
+          Apply for project to be listed on the MAGIC Firo Fund donation page.
         </label>
 
         <div className="w-full flex flex-col">
@@ -171,7 +110,7 @@ export default function Apply() {
           <label htmlFor="other_contact">Other Contact Details (if applicable)</label>
           <small>
             Please list any other relevant contact details you are comfortable sharing in case we
-            need to reach out with questions. These could include github username, twitter username,
+            need to reach out with questions. These could include GitHub username, Twitter username,
             LinkedIn, Reddit handle, other social media handles, emails, phone numbers, usernames,
             etc.
           </small>
@@ -277,7 +216,7 @@ export default function Apply() {
           The MAGIC Firo Fund may require each recipient to sign a Grant Agreement before any funds
           are disbursed. This agreement will set milestones and funds will only be released upon
           completion of milestones. In order to comply with US regulations, recipients will need to
-          identify themselves to MAGIC, in accordance with US law.
+          identify themselves to MAGIC Grants, in accordance with US law.
         </small>
 
         <Button disabled={applyMutation.isPending}>Apply</Button>
--- pages/general/apply.tsx
+++ pages/general/apply.tsx
@@ -35,88 +35,21 @@ export default function Apply() {
       <form onSubmit={handleSubmit(onSubmit)} className="max-w-5xl flex flex-col gap-4 p-4">
         <div>
           <h1>Application for General Fund Project Listing or General Fund Grant</h1>
-          <p>Thanks for your interest in the General Fund!</p>
-          {/* REVIEW START */}
+          <p>Thanks for your interest in a MAGIC Grants project!</p>
           <p>
-            We&#39;re incredibly grateful to contributors like you working to support Monero,
-            Bitcoin and other free and open source projects.
-          </p>
-          <p>
-            The MAGIC General Fund is offering a grant program and fundraising platform to support
-            Monero research and development, especially relating to privacy, security, user
-            experience, and efficiency. Proposals can be related to the Monero protocol directly, or
-            they can be related to other areas of the Monero ecosystem. For research projects,
-            please refer to special instructions
-            <Link href="/apply_research"> here</Link>.
-          </p>
-          <h2>Proposal Evaluation Criteria</h2>
-          <div>
-            Submitted proposals will be evaluated by the committee based on the following criteria:
-            <ul>
-              <li>
-                <b>Impact:</b> The proposal should have a clear impact on the Monero Project.
-              </li>
-              <li>
-                <b>Originality:</b> The proposal should be original and not a rehash of existing
-                work.
-              </li>
-              <li>
-                <b>Feasibility:</b> The proposal should be feasible to complete within the proposed
-                time frame.
-              </li>
-              <li>
-                <b>Quality:</b> The proposal should be well-written and well-organized.
-              </li>
-              <li>
-                <b>Relevance:</b> The proposal should be relevant to the Monero Project.
-              </li>
-            </ul>
-          </div>
-          {/* REVIEW END */}
-          <h2 id="Eligibility">Eligibility</h2>
-          <p>
-            All qualified researchers are eligible to apply, regardless of educational attainment or
-            occupation. However, as a nonprofit organization registered under U.S. tax laws, MAGIC
-            Grants is required to comply with certain laws when disbursing funds to grant
-            recipients. Grant recipients must complete a Due Diligence checklist, which are the last
-            two pages of{' '}
-            <a href="https://magicgrants.org/funds/MAGIC%20Fund%20Grant%20Disbursement%20Process%20and%20Requirements.pdf">
-              this document
-            </a>
-            . This includes the collection of your ID and tax information. We will conduct sanctions
-            checks.
-          </p>
-          <h2>How to Submit a Proposal</h2>
-          <p>
-            To submit a proposal, please complete the form below or create an issue on{' '}
-            {/* REVIEW START */}
-            <a href="https://github.com/MAGICGrants/Monero-Fund/issues/new?assignees=&labels=&template=grant-application.md&title=[Grant+Title]">
-              Github
-            </a>
-            {/* REVIEW END */}. Applicants are free to use their legal name or a pseudonym at this
-            step, although note the{' '}
-            <a href="#Eligibility">
-              <b>Eligibility</b>
-            </a>{' '}
-            section. You can choose to apply for a direct grant from the MAGIC General Fund&#39;s
-            General Fund and/or request that your project be listed on donate.magicgrants.org to
-            raise funds from community members.
-          </p>
-          <p>
-            Please note this form is not considered confidential and is effectively equivalent to a
-            public GitHub issue. In order to reach out privately, please send an email to{' '}
-            {fundSlugToRecipientEmail[fundSlug]}.
+            The MAGIC Grants is always interested in supporting various fundraising efforts for
+            qualifying projects and activities.
           </p>
         </div>
 
         <label className="checkbox">
           <input type="checkbox" {...register('general_fund')} />
-          Apply to receive a grant from the Magic General Fund.
+          Apply to receive a grant from MAGIC Grants.
         </label>
 
         <label className="checkbox">
           <input type="checkbox" {...register('explore_page')} />
-          Apply for project to be listed on the General Fund Donation Page.
+          Apply for project to be listed on the General Fund donation page.
         </label>
 
         <div className="w-full flex flex-col">
@@ -173,7 +106,7 @@ export default function Apply() {
           <label htmlFor="other_contact">Other Contact Details (if applicable)</label>
           <small>
             Please list any other relevant contact details you are comfortable sharing in case we
-            need to reach out with questions. These could include github username, twitter username,
+            need to reach out with questions. These could include GitHub username, Twitter username,
             LinkedIn, Reddit handle, other social media handles, emails, phone numbers, usernames,
             etc.
           </small>
@@ -232,17 +165,15 @@ export default function Apply() {
           />
         </div>
 
-        {/* REVIEW START */}
         <div className="w-full flex flex-col">
           <label htmlFor="potential_impact">Potential Impact *</label>
-          <small>Why is this project important to the Monero community?</small>
+          <small>Why is this project important to the cryptocurrency and/or privacy communities?</small>
           <textarea
             className="appearance-none block w-full text-gray-700 border rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:ring-0"
             id="potential_impact"
             {...register('potential_impact', { required: true })}
           />
         </div>
-        {/* REVIEW END */}
 
         <div className="w-full flex flex-col">
           <label htmlFor="timelines">Project Timelines and Potential Milestones *</label>
@@ -255,7 +186,7 @@ export default function Apply() {
 
         <div className="w-full flex flex-col">
           <label htmlFor="proposed_budget">
-            If you&#39;re applying for a grant from the general fund, please submit a proposed
+            If you&#39;re applying for a grant from MAGIC Grants, please submit a proposed
             budget for the requested amount and how it will be used.
           </label>
           <input
@@ -278,10 +209,10 @@ export default function Apply() {
         </div>
 
         <small>
-          The MAGIC General Fund may require each recipient to sign a Grant Agreement before any
+          MAGIC Grants may require each recipient to sign a Grant Agreement before any
           funds are disbursed. This agreement will set milestones and funds will only be released
           upon completion of milestones. In order to comply with US regulations, recipients will
-          need to identify themselves to MAGIC, in accordance with US law.
+          need to identify themselves to MAGIC Grants, in accordance with US law.
         </small>
 
         <Button disabled={applyMutation.isPending}>Apply</Button>
