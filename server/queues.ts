d0d03909e110dde2dd9bc3eb4b04bf4d0d835468
Artur <33733651+Keeqler@users.noreply.github.com>
2025-04-04 13:50:47 -0300

    feat: remove donation migration worker

--- server/queues.ts
+++ server/queues.ts
@@ -4,24 +4,13 @@ import { redisConnection as connection } from '../config/redis'
 
 import './workers/perk'
 import './workers/membership-check'
-import './workers/donation-migration'
 
-export const perkPurchaseQueue = new Queue<PerkPurchaseWorkerData>('PerkPurchase', {
-  connection,
-})
+export const perkPurchaseQueue = new Queue<PerkPurchaseWorkerData>('PerkPurchase', { connection })
 
 export const membershipCheckQueue = new Queue('MembershipCheck', { connection })
-
-export const donationMigration = new Queue('DonationMigration', { connection })
 
 membershipCheckQueue.upsertJobScheduler(
   'MembershipCheckScheduler',
   { pattern: '0 * * * *' },
   { name: 'MembershipCheck' }
-)
-
-donationMigration.upsertJobScheduler(
-  'DonationMigrationScheduler',
-  { pattern: '* * * * *' },
-  { name: 'DonationMigration' }
 )
