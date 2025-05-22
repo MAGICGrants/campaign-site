3754113fc4a2701d094eb56948f4d50f0eebd054
Artur <33733651+Keeqler@users.noreply.github.com>
2025-05-22 17:52:45 -0300

    Revert "Merge pull request #180 from MAGICGrants/funding-required-litecoin"
    This reverts commit 88ff09caa0228798bf4d93720babeb410c650a03, reversing
    changes made to f705639f4e2e10299b6afcd3746f1e7c7c19bcf7.

--- pages/api/funding-required.ts
+++ pages/api/funding-required.ts
@@ -110,9 +110,9 @@ async function handle(
 
   let responseBody: ResponseBody | ResponseBodySpecificAsset = await Promise.all(
     projects.map(async (project): Promise<ResponseBody[0]> => {
-      let bitcoinAddress: string | null = null
-      let moneroAddress: string | null = null
-      let litecoinAddress: string | null = null
+      let bitcoinAddress = ''
+      let moneroAddress = ''
+      let litecoinAddress = ''
 
       if (!project.isFunded) {
         const existingAddresses = await prisma.projectAddresses.findUnique({
