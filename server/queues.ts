3f769c7001d590171150c764128a2a4470e12424
Keeqler <33733651+Keeqler@users.noreply.github.com>
2026-03-13 20:02:44 -0300

    Schedule accounting job

--- server/queues.ts
+++ server/queues.ts
@@ -4,13 +4,22 @@ import { redisConnection as connection } from '../config/redis'
 
 import './workers/perk'
 import './workers/membership-check'
+import './workers/accounting'
 
 export const perkPurchaseQueue = new Queue<PerkPurchaseWorkerData>('PerkPurchase', { connection })
 
 export const membershipCheckQueue = new Queue('MembershipCheck', { connection })
 
+export const accountingGenerationQueue = new Queue('AccountingGeneration', { connection })
+
 membershipCheckQueue.upsertJobScheduler(
   'MembershipCheckScheduler',
   { pattern: '0 * * * *' },
   { name: 'MembershipCheck' }
+)
+
+accountingGenerationQueue.upsertJobScheduler(
+  'AccountingGenerationScheduler',
+  { pattern: '0 * * * *' },
+  { name: 'AccountingGeneration' }
 )
